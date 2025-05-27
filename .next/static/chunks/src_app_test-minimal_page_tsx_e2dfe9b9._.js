e93aaf9a4c6e6eb3a338d0ab0cf1663072ebe708
subhamindustry <i2i.industry1.@gmail.com>
2025-05-28 02:23:45 +0530

    Add .next/static/chunks/src_app_test-minimal_page_tsx_e2dfe9b9._.js 1748379225286

---
+++ .next/static/chunks/src_app_test-minimal_page_tsx_e2dfe9b9._.js
@@ -0,0 +1,40 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([typeof document === "object" ? document.currentScript : undefined, {
+
+"[project]/src/app/test-minimal/page.tsx [app-client] (ecmascript)": ((__turbopack_context__) => {
+"use strict";
+
+var { g: global, __dirname, k: __turbopack_refresh__, m: module } = __turbopack_context__;
+{
+__turbopack_context__.s({
+    "default": (()=>TestMinimal)
+});
+var __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__ = __turbopack_context__.i("[project]/node_modules/next/dist/compiled/react/jsx-dev-runtime.js [app-client] (ecmascript)");
+"use client";
+;
+function TestMinimal() {
+    return /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
+        className: "flex items-center justify-center h-screen bg-gray-50",
+        children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])("h1", {
+            className: "text-4xl font-bold text-gray-900",
+            children: "Minimal Next.js Page"
+        }, void 0, false, {
+            fileName: "[project]/src/app/test-minimal/page.tsx",
+            lineNumber: 6,
+            columnNumber: 7
+        }, this)
+    }, void 0, false, {
+        fileName: "[project]/src/app/test-minimal/page.tsx",
+        lineNumber: 5,
+        columnNumber: 5
+    }, this);
+}
+_c = TestMinimal;
+var _c;
+__turbopack_context__.k.register(_c, "TestMinimal");
+if (typeof globalThis.$RefreshHelpers$ === 'object' && globalThis.$RefreshHelpers !== null) {
+    __turbopack_context__.k.registerExports(module, globalThis.$RefreshHelpers$);
+}
+}}),
+}]);
+
+//# sourceMappingURL=src_app_test-minimal_page_tsx_e2dfe9b9._.js.map
