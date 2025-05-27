90e0cf5590d7a0aa325233144d26229cde7aafa8
subhamindustry <i2i.industry1.@gmail.com>
2025-05-28 02:23:16 +0530

    Add .next/static/chunks/src_app_page_tsx_8af44de9._.js 1748379195379

---
+++ .next/static/chunks/src_app_page_tsx_8af44de9._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_5d1cf27e._.js"
+],
+    source: "dynamic"
+});
