7dad869fd1cf07a9ef626093b8cfe4ace71ae709
subhamindustry <i2i.industry1.@gmail.com>
2025-05-28 02:23:43 +0530

    Add .next/static/chunks/src_app_test-minimal_page_tsx_24ab3647._.js 1748379222594

---
+++ .next/static/chunks/src_app_test-minimal_page_tsx_24ab3647._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_app_test-minimal_page_tsx_e2dfe9b9._.js"
+],
+    source: "dynamic"
+});
